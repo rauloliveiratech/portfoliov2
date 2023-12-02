589bf317849536b162b605713b50fde33a53e24a
rauloliveiratech <rauloliveiratech@gmail.com>
2023-12-01 21:34:41 -0300

    fix

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -4,10 +4,8 @@ const ProjectsContainer = () => {
     return (
         <section className='projects-container'>
             <h2>Projetos 📂</h2>
-            <p>Alguns projetos que criei, colaborei e também alguns que estão em processo 
+            <p>Alguns projetos que criei, colaborei e também alguns que estão em processo</p>
             <a href="https://github.com/rauloliveiratech?tab=repositories" className='btn' target='_blank'>Ver Projetos</a>
-            </p>
-        
         </section>
     );
 };
